12020ccdca2938d7246a5a133c8027d0aca3a707
SilverfoxFR <135644897+SilverfoxFR@users.noreply.github.com>
2025-10-15 12:07:43 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -11,27 +11,6 @@ function registerSW() {
 // call it first thing
 registerSW();
 
-document.getElementById('flushCacheBtn').addEventListener('click', async () => {
-  try {
-
-    const cacheNames = await caches.keys();
-    await Promise.all(cacheNames.map(name => caches.delete(name)));
-    console.log('All caches deleted');
-
-  
-    const registrations = await navigator.serviceWorker.getRegistrations();
-    for (const reg of registrations) {
-      await reg.unregister();
-      console.log('🧹 Service worker unregistered');
-    }
-
-    // 🔹 3. Reload the page to start clean
-    window.location.reload(true);
-
-  } catch (err) {
-    console.error('Error flushing cache:', err);
-  }
-});
 
 
 console.log("main.js running!");
